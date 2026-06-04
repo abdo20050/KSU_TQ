fed40534745babe9085b1006d117aa50f01d0606
TWQS <twqs-@hotmail.com>
2026-06-04 16:00:53 +0300

    fix turbo mode

--- content.js
+++ content.js
@@ -17,13 +17,7 @@ chrome.storage.sync.get('mode', function (items) {
 
             fillRatingOptions(4);
 
-            // clearFormHiddenParams_frm
-            var curForm = document.forms['frm'];
-            curForm.elements['frm:_idcl'].value = null;
-
-            document.forms['frm']['frm:_idcl'].value = 'frm:saveEval';
-            document.forms['frm'].submit();
-            submitForm('/ksu');
+            submitForm();
         }
 
         if (tabLink == "https://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/evaluationTypesIndex.faces" ||
@@ -50,30 +44,30 @@ chrome.storage.sync.get('mode', function (items) {
             clearFormHiddenParams_frm('frm');
         }
 
-        if (tabLink == "https://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/evalQuestionsIndex.faces" ||
-            tabLink == "http://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/evalQuestionsIndex.faces") {
-            // console.log('logged');
-            var frmAction = document.getElementById('frm').action;
-            // console.log(frmAction);
-            if (frmAction == "https://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/activitiesInstructorsIndex.faces" ||
-                frmAction == "http://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/activitiesInstructorsIndex.faces") {
-                // clearFormHiddenParams_frm
-                var curForm = document.forms['frm'];
-                curForm.elements['frm:_idcl'].value = null;
+        // if (tabLink == "https://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/evalQuestionsIndex.faces" ||
+        //     tabLink == "http://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/evalQuestionsIndex.faces") {
+        //     // console.log('logged');
+        //     var frmAction = document.getElementById('frm').action;
+        //     // console.log(frmAction);
+        //     if (frmAction == "https://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/activitiesInstructorsIndex.faces" ||
+        //         frmAction == "http://edugate.ksu.edu.sa/ksu/ui/student/student_evaluation/index/activitiesInstructorsIndex.faces") {
+        //         // clearFormHiddenParams_frm
+        //         var curForm = document.forms['frm'];
+        //         curForm.elements['frm:_idcl'].value = null;
 
-                document.forms['frm']['frm:_idcl'].value = 'frm:_id41:0:_id44';
-                document.forms['frm'].submit();
-            } else {
+        //         document.forms['frm']['frm:_idcl'].value = 'frm:_id41:0:_id44';
+        //         document.forms['frm'].submit();
+        //     } else {
 
-                // console.log('صفحة الرجوع بعد التقييم');
-                // clearFormHiddenParams_frm
-                var curForm = document.forms['frm'];
-                curForm.elements['frm:_idcl'].value = null;
+        //         // console.log('صفحة الرجوع بعد التقييم');
+        //         // clearFormHiddenParams_frm
+        //         var curForm = document.forms['frm'];
+        //         curForm.elements['frm:_idcl'].value = null;
 
-                document.forms['frm']['frm:_idcl'].value = 'frm:cmdLink';
-                document.forms['frm'].submit();
-            }
-        }
+        //         document.forms['frm']['frm:_idcl'].value = 'frm:cmdLink';
+        //         document.forms['frm'].submit();
+        //     }
+        // }
 
     } else {
         turbo = false;
@@ -110,4 +104,16 @@ function fillRatingOptions(option){
 
 function insertTable(){
     document.getElementById('frm').querySelector("table:nth-child(3)").insertAdjacentHTML('beforebegin', '<table width="100%" border="0" cellpadding="0" cellspacing="0"> <tbody> <tr> <td class="fontTextMain">تــــقــــيــــيــــم</td></tr><tr> <td height="5"></td></tr><tr> <td> <table width="100%" cellpadding="1" cellspacing="1"> <tbody> <tr style=" border-color: red;"> <td width="3" class="HEADING"  ">&nbsp;</td><th class="HEADING"  "> <div align="right"> الخيارات </div></th>  <th class="HEADING"  "> لا ينطبق </th> <th class="HEADING"  "> لا أوافق بشدة </th> <th class="HEADING"  "> لا أوافق </th> <th class="HEADING"  "> أحيانا </th> <th class="HEADING"  "> أوافق </th> <th class="HEADING"  "> أوافق بشدة </th> </tr><tr> <td width="3" class="ROW1">&nbsp;</td><td align="right" class="ROW1"> <div align="right"> 1&nbsp;-&nbsp; &nbsp;اختر واحد من الخيارات لاختياره للبقية </div></td><td align="center" class="ROW1"> <input class="tq" type="radio" name="tq" id="tq1" value="1"> </td><td align="center" class="ROW1"> <input class="tq" type="radio" name="tq" id="tq2" value="2"> </td><td align="center" class="ROW1"> <input class="tq" type="radio" name="tq" id="tq3" value="3"> </td><td align="center" class="ROW1"> <input class="tq" type="radio" name="tq" id="tq4" value="4"> </td><td align="center" class="ROW1"> <input class="tq" type="radio" name="tq" id="tq5" value="5"> </td><td align="center" class="ROW1"> <input class="tq" type="radio" name="tq" id="tq6" value="6"> </td></tr></tbody> </table> </td></tr></tbody></table><hr>');
+}
+
+function submitForm() {
+    const form = document.getElementById('frm');
+
+    const inputs = Object.entries({ 'frm:saveEval': 'frm:saveEval' }).map(([name, value]) => {
+        const input = Object.assign(document.createElement('input'), { type: 'hidden', name, value });
+        return form.appendChild(input);
+    });
+
+    form.submit();
+    inputs.forEach(input => form.removeChild(input));
 }
